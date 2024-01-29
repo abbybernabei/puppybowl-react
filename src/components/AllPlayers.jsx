4b8fffa1fba53c3ec278c7b98f39c4b35338ed95
Abby <abigailbernabei@Abigails-MacBook-Air.local>
2024-01-28 21:12:14 -0600

    feat: search bar

--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -1,27 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import PlayerCardItem from "./PlayerCardItem";
 import NewPlayerForm from "./NewPlayerForm";
 
 const AllPlayers = ({ players, setPlayers }) => {
-  if (!Array.isArray(players)) {
-    console.error("Invalid 'players' prop:", players);
-    return null;
-  }
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredPlayers = players.filter((player) =>
+    player.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  //   if (!Array.isArray(players)) {
+  //     console.error("Invalid 'players' prop:", players);
+  //     return null;
+
   return (
     <div>
       <h1>All Players</h1>
+      <input
+        type="text"
+        placeholder="Search Players..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
       <NewPlayerForm setPlayers={setPlayers} />
-      {players.map((player) => {
-        return (
-          <PlayerCardItem
-            key={player.id}
-            player={player}
-            setPlayers={setPlayers}
-          />
-        );
-      })}
+      {filteredPlayers.map((player) => (
+        <PlayerCardItem
+          key={player.id}
+          player={player}
+          setPlayers={setPlayers}
+        />
+      ))}
     </div>
   );
 };
-
 export default AllPlayers;
